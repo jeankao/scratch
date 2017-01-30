08207c2d5d7220be75f15000982ed8075b3af85f
t005 <t005@mail.nksh.tp.edu.tw>
2017-01-30 04:46:42 -0500

    update

--- scratch/production_settings.py
+++ scratch/production_settings.py
@@ -2,15 +2,23 @@
 
 # Import all default settings.
 from .settings import *
-from django.conf import settings
 import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config()
 }
 
-# Static asset configuration.
-STATIC_ROOT = 'static'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure().
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
