24309b38ee0a9f495ec84b0e4e9cb7b2834e5baf
t005 <t005@mail.nksh.tp.edu.tw>
2017-01-30 05:10:27 -0500

    update

--- scratch/settings.py
+++ scratch/settings.py
@@ -134,20 +134,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-PROJECT_ROOT = BASE_DIR
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-#STATIC_ROOT = 'static'
+
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"), 
+]
 
 # Static asset configuration.
-#STATIC_ROOT = 'static'
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 import os
 if os.getenv('DATABASE_URL') is not None:
